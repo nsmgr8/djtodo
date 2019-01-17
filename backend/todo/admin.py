518b0d761df7f1b0af76297777fccf54022b2b4e
M Nasimul Haque <nasim.haque@appliansys.com>
2019-01-17 23:36:00 +0000

    add task to admin

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fields = ('name', 'description', 'status', ('created_by', 'created_on'))
+    readonly_fields = ('created_on', 'created_by')
+
+    list_display = ('name', 'created_by', 'created_on', 'status')
+    list_filter = ('status', 'created_by', 'created_on')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+    def has_delete_permission(self, request, obj=None):
+        return obj and request.user == obj.created_by

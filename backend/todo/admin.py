6cb7ff9c0c66cca1022d86cba78dae93820bc5c6
M Nasimul Haque <nasim.haque@appliansys.com>
2019-01-18 00:38:57 +0000

    allow others to mark task done

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -6,10 +6,20 @@ from .models import Task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     fields = ('name', 'description', 'status', ('created_by', 'created_on'))
-    readonly_fields = ('created_on', 'created_by')
+    own_readonly_fields = ('created_on', 'created_by')
+    others_readonly_fields = ('name', 'description') + own_readonly_fields
 
     list_display = ('name', 'created_by', 'created_on', 'status')
     list_filter = ('status', 'created_by', 'created_on')
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj and obj.created_by != request.user:
+            self.readonly_fields = self.others_readonly_fields
+            if obj.status:
+                self.readonly_fields = self.readonly_fields + ('status',)
+        else:
+            self.readonly_fields = self.own_readonly_fields
+        return super().get_form(request, obj, **kwargs)
 
     def save_model(self, request, obj, form, change):
         if not change:

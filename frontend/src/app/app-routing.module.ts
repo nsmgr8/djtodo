28564a368be1b3edffb3deacd4856235b0a53fc8
M Nasimul Haque <nasim.haque@appliansys.com>
2019-01-20 12:55:36 +0000

    init tasks pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskCreateComponent } from './task-create/task-create.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: '/tasks', pathMatch: 'full'},
+    {path: 'tasks', component: TaskListComponent},
+    {path: 'create', component: TaskCreateComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskCreateComponent } from './task-create/task-create.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        TaskListComponent,
+        TaskCreateComponent
     ],
     imports: [
         BrowserModule,

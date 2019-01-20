1cf74c2c66f7e79e1c3c11e83466f93626e184f8
M Nasimul Haque <nasim.haque@appliansys.com>
2019-01-20 19:21:37 +0000

    setup ng auth guard, interceptor

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './services/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskCreateComponent } from './task-create/task-create.component';
@@ -8,7 +9,7 @@ import { TaskCreateComponent } from './task-create/task-create.component';
 const routes: Routes = [
     {path: '', redirectTo: '/tasks', pathMatch: 'full'},
     {path: 'tasks', component: TaskListComponent},
-    {path: 'create', component: TaskCreateComponent},
+    {path: 'create', component: TaskCreateComponent, canActivate: [AuthGuard]},
     {path: 'login', component: LoginComponent},
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
+import { AuthInterceptor } from './services/auth.guard';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
@@ -27,7 +28,9 @@ import { LoginComponent } from './login/login.component';
 
         AppRoutingModule
     ],
-    providers: [],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }

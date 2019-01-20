82ed945e4d75ede7f01dac52fdecd9ffa0b6c1f5
M Nasimul Haque <nasim.haque@appliansys.com>
2019-01-20 14:31:15 +0000

    expose tasks api using rest framework

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, serializers
 
-# Create your views here.
+from .models import Task
+
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('name', 'description', 'status')
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer

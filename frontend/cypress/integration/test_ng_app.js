fcddd80707b19a6959aa9dc3420b3fb189e4cf21
M Nasimul Haque <nasim.haque@appliansys.com>
2019-01-25 20:13:06 +0000

    test creating a task

--- frontend/cypress/integration/test_ng_app.js
+++ frontend/cypress/integration/test_ng_app.js
@@ -27,4 +27,50 @@ describe('angular UI tests', () => {
         cy.visit(url('foo-bar'));
         cy.get('app-no-content').contains('page does not exist');
     });
+
+    describe('authenticated acces', () => {
+        beforeEach(() => {
+            cy.server();
+            cy.route('/api/whoami/').as('whoami');
+            cy.route('/api/is_authenticated/').as('is_authenticated');
+            cy.route('POST', '/api/login/').as('login');
+
+            cy.visit(url(''));
+            cy.get('#id_username').type('admin');
+            cy.get('#id_password').type('password{enter}');
+
+            cy.get('@whoami').then(xhr => {
+                expect(xhr.status).to.equal(200);
+                expect(Object.keys(xhr.responseBody).length).to.equal(0);
+            });
+            cy.get('@is_authenticated').then(xhr => {
+                expect(xhr.status).to.equal(401);
+            });
+            cy.wait('@login').then(xhr => {
+                expect(xhr.status).to.equal(200);
+            });
+        });
+
+        it.only('create a task', () => {
+            cy.server();
+            cy.route('/api/whoami/').as('whoami');
+            cy.route('POST', '/api/tasks/').as('tasks');
+
+            cy.visit(url('create'));
+
+            cy.get('@whoami').then(xhr => {
+                expect(xhr.status).to.equal(200);
+                expect(xhr.responseBody.username).to.equal('admin');
+            });
+
+            cy.get('#id_name').type('Task 1');
+            cy.get('#id_description').type('task 1 description');
+            cy.get('button[type="submit"]').click();
+
+            cy.wait('@tasks').then(xhr => {
+                expect(xhr.status).to.equal(201);
+            });
+            cy.url().should('contain', '/tasks');
+        });
+    });
 });
